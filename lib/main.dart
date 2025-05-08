8d18ae6e24db7c3024fddfe7d1e0a5a7ec863c7a
TavGit <gustavbaim18@gmail.com>
2025-05-08 12:05:40 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Aplikasi Pos by Gustav'),
+      home: const MyHomePage(title: 'Aplikasi Pos Kasir by Gustav'),
     );
   }
 }
